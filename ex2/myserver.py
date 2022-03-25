bb4b6731639b46c90282ccd851f156d099e6d62d
Joseph Higgitt <joseph.higgitt@student.manchester.ac.uk>
2022-03-25 17:13:23 +0000

    Server can count number of active clients

--- ex2/myserver.py
+++ ex2/myserver.py
@@ -2,6 +2,17 @@ import sys
 from ex2utils import Server
 
 class Server(Server):
+	numConnections = 0
+
+	def getNum(self):
+		return Server.numConnections
+
+	def inc(self):
+		Server.numConnections += 1
+
+	def dec(self):
+		Server.numConnections -= 1
+
 
 	def onStart(self):
 		print("Server has started")
@@ -11,6 +22,8 @@ class Server(Server):
 
 	def onConnect(self, socket):
 		print("Server has connected")
+		self.inc()
+		print("Server has " + str(self.getNum()) + " active connections")
 
 	def onMessage(self, socket, message):
 		print("Message has been recieved")
@@ -18,6 +31,8 @@ class Server(Server):
 
 	def onDisconnect(self, socket):
 		print("Server has disconnected")
+		self.dec()
+		print("Server has " + str(self.getNum()) + " active connections")
 
 # Parse the IP address and port you wish to listen on.
 ip = sys.argv[1]
